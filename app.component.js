8a88cb7e90e943894f9bae276fd66ba52c02ecd1
chandan <bhartichandankr@gmail.com>
2018-02-08 23:58:28 -0800

    x

--- app.component.js
+++ app.component.js
@@ -1,4 +1,8 @@
 var myapp=angular.module('myapp');
 myapp.component('studentDetails',{
-    template :'<div>chandan Details</div>'
+    templateUrl :'student-details.tlp.html',
+    //controllerAs:'vm',
+    controller: [function(){
+        this.myname='custom component';
+    }]
 });
