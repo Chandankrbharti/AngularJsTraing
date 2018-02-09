4632627939636bb48bd7649f5b6d8867d92ea8b0
chandan <bhartichandankr@gmail.com>
2018-02-08 23:43:34 -0800

    x

--- app.component.js
+++ app.component.js
@@ -2,6 +2,10 @@ var myapp=angular.module('myapp');
 myapp.component('studentDetails',{
     templateUrl :'student-details.tlp.html',
     //controllerAs:'vm',
+    bindings:{
+        myTitle : '@', //string
+        myList : '<' //properties
+    },
     controller: [function(){
         this.myname='custom component';
     }]
