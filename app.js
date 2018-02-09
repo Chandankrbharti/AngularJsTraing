0d2cb81baa5e733db051a78146a06a77e6379bc1
chandan <bhartichandankr@gmail.com>
2018-02-08 23:57:54 -0800

    x

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-var myapp=angular.module('myapp',[]);
+var myapp=angular.module('myapp',['ui.router']);
+myapp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
+    $stateProvider.state({
+        name:'home',
+        url:'/home',
+        template:'<div>This is the home page</div>'
+    });
+    $stateProvider.state({
+        name:'about',
+        url:'/about',
+        template:'<div>This is the home page</div>'
+    });
+    $urlRouterProvider.otherwise('/home');
+}]);
 myapp.controller('MyCtrl',['$scope','AppService',function($scope,AppService){
 
 AppService.getStudents().then(function(result){
