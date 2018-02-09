97aa44d13ce960839b6ccd51c9b20df54d409704
chandan <bhartichandankr@gmail.com>
2018-02-09 00:35:27 -0800

    service

--- app.service.js
+++ app.service.js
@@ -1,16 +1,7 @@
 var myapp=angular.module('myapp');
 myapp.service('AppService',[function(){
-    var students=[
-        {
-            name:"chanan",
-            age:25
-        },
-        {
-            name :"chandani",
-            age :22
-        }
-    ];
+   
     this.getStudents=function(){
-        return students;
+              return $http.get("/student.json");
     };
 }]);
